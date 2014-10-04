0d2dc52e6d1c6765dfdfc484e500f47470c2a19a
Sachin Jain <sachingvit@gmail.com>
2014-10-04 13:40:32 +0530

    valid email id
    this script is can be use in login or registration form or anywhere
    where we need a valid email (format)
    
    eg.  abc@xyz.com

---
+++ valid-email-js/email-valid.js
@@ -0,0 +1,22 @@
+/*
+ * This is sample code to validate email address on input box blur
+ * 
+ *
+ */
+
+
+$(function(){
+        
+    // .username is class of that input box
+        
+    $('.username').blur(function(){
+        var current_var = $(this); 
+        var email = current_var.val();
+        
+        // checking for proper email format eg. abc@xyz.com
+        if('anbsadb'){
+            
+        }
+    });
+        
+});
